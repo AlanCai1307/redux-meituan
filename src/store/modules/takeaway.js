164887a17b9528baea3e4229a79c094772b0ad79
AlanCai <p_alanccai@tencent.com>
2023-11-08 17:48:18 +0800

    添加购物车实现

--- src/store/modules/takeaway.js
+++ src/store/modules/takeaway.js
@@ -6,9 +6,12 @@ const foodStore = createSlice({
 
   // 定义初始值
   initialState: {
+    // 商品列表
     foodsList: [],
     // 菜单激活下标值
-    activeIndex: 0
+    activeIndex: 0,
+    // 购物车列表
+    cartList: []
   },
   // 定义方法
   reducers: {
@@ -18,11 +21,21 @@ const foodStore = createSlice({
     // 更改activeIndex
     changeActiveIndex (state, action) {
       state.activeIndex = action.payload
+    },
+    // 添加购物车
+    addCart (state, action) {
+      // 是否添加过
+      const item = state.cartList.find(item => item.id === action.payload.id)
+      if (item) {
+        item.count++
+      } else {
+        state.cartList.push(action.payload)
+      }
     }
   }
 })
 
-const {setFoodsList,changeActiveIndex} = foodStore.actions
+const {setFoodsList,changeActiveIndex,addCart} = foodStore.actions
 const fetchFoodsList = () => {
   return async dispatch => {
     const res = await axios.get('http://localhost:3004/takeaway')
@@ -30,6 +43,6 @@ const fetchFoodsList = () => {
   }
 }
 
-export {fetchFoodsList,changeActiveIndex }
+export {fetchFoodsList,changeActiveIndex,addCart }
 const reducer = foodStore.reducer
 export default reducer
